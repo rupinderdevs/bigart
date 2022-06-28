324ff2b6733c15ae7a7da97ad5f88a60f7d03c5d
Rupinder Singh <inforupinder79@gmail.com>
2022-06-28 19:45:57 +0530

    update v

--- pages/audition.js
+++ pages/audition.js
@@ -120,7 +120,7 @@ export default function Audition() {
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                   htmlFor="user_avatar"
                 >
-                  Upload Audio Not More Than 1 Minute
+                 Video Should Not More Than 1 Minute
                 </label>
                 <input
                   className=" w-full m-auto table text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
