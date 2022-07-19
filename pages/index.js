7f992f8633ee0d1298c182659a44723d540cbf1b
Rupinder Singh <inforupinder79@gmail.com>
2022-07-19 20:31:59 +0530

    coming soon page

--- pages/index.js
+++ pages/index.js
@@ -63,12 +63,12 @@ export default function Home() {
           className="inset-0 h-screen bg-cover bg-center bg-[url('../images/bg.jpg')]"
         ></div>
         <div className="absolute inset-0 z-20 flex items-center justify-center h-screen w-full bg-gray-900 bg-opacity-75" />
-        <div className="absolute inset-0  z-30  flex flex-col items-center justify-center">
+        <div className="absolute inset-0  z-30  flex flex-col items-center my-8 justify-safe lg:justify-center">
           <div
             className="shadow-2xl rounded-lg w-4/5 h-96 bg-cover bg-center bg-[url('../images/bg.jpg')]"
           >
             <div className="grid grid-cols-12 gap-1">
-              <div className="relative my-6 px-8 col-span-12 sm:col-span-12 sm:mb-12 md:col-span-7 lg:col-span-7 xxl:col-span-7">
+              <div className="relative my-6 pb-40 px-8 col-span-12 sm:col-span-12 sm:mb-12 md:col-span-7 lg:col-span-7 xxl:col-span-7">
                 <div className="border-l-4 border-gray-400 md:py-20 py-5 px-5 mx-2 absolute left-0">
                   <p className="italic text-white text-xl md:text-4xl lg:text-6xl uppercase text-center font-semibold mb-6">
                     Big Art <Image className='w-32'
